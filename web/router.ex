0ca427da6ab5bc184080688291eb5afb9552f881
Evandro Junior <evandrojr@gmail.com>
2016-06-01 17:57:00 -0300

    cadastrando tarefas

--- web/router.ex
+++ web/router.ex
@@ -16,7 +16,9 @@ defmodule Mechanize.Router do
   scope "/", Mechanize do
     pipe_through :browser # Use the default browser stack
 
-    get "/", PageController, :index
+    # get "/", PageController, :index
+    get "/", TarefaController, :index
+    resources "/tarefas", TarefaController
   end
 
   # Other scopes may use custom stacks.
